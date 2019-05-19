4773ff49c42fa585fe657991ff957641c9c768a1
Muhriddin Ismoilov <ismoilovmuh1996@gmail.com>
2019-05-19 11:17:18 +0500

    detail view added

--- news/views.py
+++ news/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import News
 
 class NewsList(ListView):
     model = News
+    queryset = News.objects.filter(is_published=True).all()
     template_name = 'news/news_list.html'
+
+
+class NewsDetail(DetailView):
+    model = News
+    template_name = 'news/news_detail.html'
+    context_object_name = 'news'
+    
 
 
