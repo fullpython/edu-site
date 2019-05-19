0c2e015f662444f86ed4acd3152cc0e4c11c4a60
Muhriddin Ismoilov <ismoilovmuh1996@gmail.com>
2019-05-19 10:54:54 +0500

    listview added

--- news/views.py
+++ news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import News
 
-# Create your views here.
+class NewsList(ListView):
+    model = News
+    template_name = 'news/news_list.html'
+
+
