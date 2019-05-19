f1bdf7ce1f29ea0fb8ec03c2d961a2538e58e894
Muhriddin Ismoilov <ismoilovmuh1996@gmail.com>
2019-05-19 10:25:06 +0500

    News model added

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title','is_published','date_published']
+
+admin.site.register(News,NewsAdmin)
+#admin.site.register()
--- news/models.py
+++ news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=100,verbose_name='Sarlavha')
+    body = models.TextField()
+
+    date_published = models.DateTimeField(auto_now_add=True,verbose_name='Yaratilgan Vaqt')
+
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title}'
