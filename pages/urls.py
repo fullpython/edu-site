ee84d1f2d012133cfc070c46ed83bb06a23e0db7
Muhriddin Ismoilov <ismoilovmuh1996@gmail.com>
2019-05-27 08:36:11 +0500

    formview added

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 
-from .views import (HomeView)
+from .views import (
+    HomeView,
+    AboutView,
+)
 
 urlpatterns = [
-    path('',HomeView.as_view(),name='home')
+    path('',HomeView.as_view(),name='home'),
+    path('about/',AboutView.as_view(),name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
-
+from .forms import ContactUsForm
+from .models import ContactUs
 
 class HomeView(TemplateView):
     template_name='base.html'
+
+class AboutView(FormView):
+    template_name = 'pages/about.html'
+    form_class = ContactUsForm
+
+    success_url = '/'
+
+    def form_valid(self,form):
+        form.save()
+        return super().form_valid(form)
+    
+   
+
+    
